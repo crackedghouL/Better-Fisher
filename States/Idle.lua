4cbcc64bd96abf6461716d732968282abbeb3472
naski :) <crackedghouL@gmail.com>
2016-06-14 16:36:46 +0200

    Idle state WAIT command

--- States/Idle.lua
+++ States/Idle.lua
@@ -18,4 +18,10 @@ function IdleState:NeedToRun()
 end
 
 function IdleState:Run()
+	if GetSelfPlayer():CheckCurrentAction("RUN_SPRINT") or GetSelfPlayer():CheckCurrentAction("RUN")
+	or GetSelfPlayer():CheckCurrentAction("RUN_ING") or GetSelfPlayer():CheckCurrentAction("RUN_STOP")
+	or GetSelfPlayer():CheckCurrentAction("RIN_SPRINT_FAST") or GetSelfPlayer():CheckCurrentAction("RUN_SPRINT_FAST_ST")
+	or GetSelfPlayer():CheckCurrentAction("RUN")
+			then GetSelfPlayer():DoActionAtPosition("WAIT",GetSelfPlayer().CrosshairPosition, 1)
+		end
 end
